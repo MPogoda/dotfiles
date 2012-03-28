837657b62e0f9f2b0ffa686b9c0eef3e33d669a8
Michael Pogoda <mpogoda@lavabit.com>
2012-03-29 01:58:22 +0300

    disable led notification of keyboard layout change, changed model to pc86

--- awesome/scripts/autorun.sh
+++ awesome/scripts/autorun.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 wmname LG3D
-setxkbmap -layout us,ru -option grp:caps_toggle,grp_led:caps,grp_led:scroll,grp_led:num -model pc105
+setxkbmap -layout us,ru -option grp:caps_toggle -model pc86
 pgrep -x urxvtd >/dev/null || urxvtd --opendisplay --quiet --fork
