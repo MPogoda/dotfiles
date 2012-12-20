155e52fd7646e4ddb5c683eeb9decea9f8ebe7f1
Michael Pogoda <do0dlez1991@gmail.com>
2012-12-20 02:11:10 +0200

    Start all the stuff from .xinitrc, not from autorun.sh

--- awesome/scripts/autorun.sh
+++ awesome/scripts/autorun.sh
@@ -1,6 +1,1 @@
 #!/bin/sh
-
-wmname LG3D
-setxkbmap -layout us,ru -option grp:caps_toggle -model pc86
-pgrep -x urxvtd >/dev/null || urxvtd --opendisplay --quiet --fork
-pgrep -x kbdd >/dev/null || kbdd
