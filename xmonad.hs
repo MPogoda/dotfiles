a3f51c26c21fbffab9ef60c27f09f20d4d01b6e7
Michael Pogoda <do0dlez1991@gmail.com>
2013-01-21 02:38:48 +0200

    Rewritted xmonad config:
      * use global functions instead of ones in where statement
      * added namedScratchpads
      * removed xf86 keys from shortcuts: I'd prefer to use acpid
      * removed startupHook. It's bad
      * added comments
      * tweaked pretty printing to statusbar:
        - do not show NSP workspace
        - do not show title
        - short names for layouts
        - proper separator
      * use full layout by default
      * rewritten managehook: use more versatile way for shifting
      * added types for functions

--- xmonad.hs
+++ xmonad.hs
@@ -1,15 +1,22 @@
 import Data.List            (isPrefixOf)
 import Control.Applicative  ((<$>))
+-- for exiting xmonad
 import System.Exit          (exitWith, ExitCode(..))
 
-import XMonad                       hiding (Tall(..))
-import XMonad.Actions.WindowGo      (runOrRaise)
-import XMonad.Hooks.DynamicLog      (statusBar, xmobarPP, xmobarColor, ppCurrent, wrap)
+import XMonad
+-- for statusbar
+import XMonad.Hooks.DynamicLog      (statusBar, xmobarPP, xmobarColor, PP(..), wrap)
 import XMonad.Hooks.EwmhDesktops    (ewmh, fullscreenEventHook)
 import XMonad.Hooks.ManageDocks     (docksEventHook, ToggleStruts(..))
-import XMonad.Layout.NoBorders      (smartBorders)
-import XMonad.Layout.ResizableTile  (ResizableTall(..), MirrorResize(..))
+-- honor size hints
 import XMonad.Layout.LayoutHints    (layoutHintsToCenter, hintsEventHook)
+-- auto-borders
+import XMonad.Layout.NoBorders      (smartBorders)
+-- enable resizing for non-master windows
+import XMonad.Layout.ResizableTile  (ResizableTall(..), MirrorResize(..))
+-- hiding & showing some windows
+import XMonad.Util.NamedScratchpad  (namedScratchpadAction, namedScratchpadManageHook, NamedScratchpad(..), nonFloating, customFloating)
+-- easy way to specify shortcuts (including emacs-like submaps)
 import XMonad.Util.EZConfig         (mkKeymap)
 
 import qualified XMonad.StackSet as W
@@ -20,135 +27,164 @@ main = do
   xmonad =<< statusBar "xmobar" myPP toggleStrutsKey defaults
   where
     defaults = ewmh defaultConfig
-      { terminal           = "urxvtc"
-      , borderWidth        = 5
-      , modMask            = mod4Mask
-      , workspaces         = words "a s d f z x c v"
-      , normalBorderColor  = darkGray
-      , focusedBorderColor = darkOrange
-      , layoutHook         = layoutHintsToCenter . smartBorders $ myLayout
-      , manageHook         = myManageHook
-      , handleEventHook    = myEventHook
-      , keys               = myKeys
-      , startupHook        = myStartup
-      }
-    -- Layouts -----------------------------------
-    myLayout = tiled ||| Mirror tiled ||| Full
+              { terminal           = myTerminal
+              , borderWidth        = 5
+              , modMask            = mod4Mask
+              , workspaces         = myWorkspaces
+              , normalBorderColor  = darkGray
+              , focusedBorderColor = darkOrange
+              , layoutHook         = layoutHintsToCenter . smartBorders $ myLayout
+              , manageHook         = myManageHook <+> namedScratchpadManageHook scratchpads
+              , handleEventHook    = myEventHook
+              , keys               = myKeys
+              }
+
+-- xmobar pretty printing stuff
+myPP :: PP
+myPP = xmobarPP { ppCurrent = xmobarColor orange "" . wrap "<" ">"
+                -- separator between elements
+                , ppSep     = xmobarColor orange "" " | "
+                -- order in which we have to show elements (title is ignored)
+                , ppOrder   = \(ws:l:_) -> [l, ws]
+                -- show only workspaces that are predefined by me
+                , ppHidden  = \w -> if elem w myWorkspaces then w else ""
+                -- short 'titles' for layouts
+                , ppLayout  = \n -> case n of
+                                "Full" -> "[ ]"
+                                "ResizableTall" -> "[|]"
+                                "Mirror ResizableTall" -> "[-]"
+                                _      -> n
+
+                }
+-- dummy line. needed for statusBar function.
+toggleStrutsKey XConfig { XMonad.modMask = modMask } = (modMask, xK_b)
+
+-- terminal i'm using
+myTerminal :: String
+myTerminal = "urxvtc"
+
+-- list of workspaces
+myWorkspaces :: [String]
+myWorkspaces = words "a s d f g z x c v"
+
+-- Layouts
+myLayout = Full ||| tiled ||| Mirror tiled
+  where
     tiled   = ResizableTall nmaster delta ratio []
     nmaster = 1     -- number of windows in master pane
-    ratio   = toRational (2/(1+sqrt(5)::Double))
+    ratio   = toRational (2/(1+sqrt(5)::Double)) -- phi
     delta   = 3/100 -- step of increasing
 
-    -- Manage windows
-    myManageHook = composeAll
-      (
-      [ myIgnores --> doIgnore -- Don't manage
-      , myFloats  --> doFloat  -- Make floating
-      ]
-      ++
-      -- shifting to workspace based on window class
-      [ className =? x --> doShift w | (x, w) <- clsShifts ]
-      ++
-      -- shifting to workspace based on window name
-      [ resource =? x --> doShift w  | (x, w) <- rcsShifts ]
-      )
+-- How we should manage windows
+myManageHook :: ManageHook
+myManageHook = composeAll
+            (
+            [ myIgnores --> doIgnore ] -- Don't manage
+            ++
+            [ myFloats  --> doFloat  ] -- Make floating
+            ++
+            [ x --> doShift w | (x, w) <- myShifts ] -- Perform shifts
+            )
+  where
+    -- list of conditions when we have make window floating
     myFloats = foldr1 (<||>)
       [ ("Figure" `isPrefixOf`) <$> title
       , className =? "feh"
       , className =? "mplayer2"
       , className =? "Steam"
       ]
+    -- list of conditions when we have to ignore window (don't manage it)
     myIgnores = foldr1 (<||>)
       [ resource  =? "panel"
-      , resource  =? "trayer"
       , className =? "stalonetray"
       ]
+    -- when condition from first part of tuple succeed, move that window to #2
+    myShifts = map (\(x, y) -> (className =? x, y)) clsShifts
     -- [ ( className, workspace) ]
     clsShifts = [ ("Chromium-browser", "s")
                 , ("Leechcraft", "a")
                 , ("Djview", "z")
                 , ("Okular", "z")
                 , ("Kchmviewer", "z")
-                , ("Steam", "v")
                 ] :: [(String,  String)]
-    rcsShifts = [ ("ncmpc", "d")
-                ] :: [(String, String)]
 
-    -- eventHook
-    myEventHook = docksEventHook <+> hintsEventHook <+> fullscreenEventHook
+-- eventHook
+myEventHook = docksEventHook <+> hintsEventHook <+> fullscreenEventHook
 
-    -- xmobar staff
-    myPP = xmobarPP { ppCurrent = xmobarColor orange "" . wrap "<" ">" }
-    toggleStrutsKey XConfig { XMonad.modMask = modMask } = (modMask, xK_b)
+-- Keys
+myKeys = \conf -> mkKeymap conf $
+         [ ("M-<Return>",   spawn       $ myTerminal)
+         , ("M-C-<Esc>",    spawn       $ "xkill")
+         , ("M-r",          spawn       $ "dmenu_run")
+         -- cycle through all possible layouts
+         , ("M-<Space>",    sendMessage $ NextLayout)
+         -- restore default layout
+         , ("M-S-<Space>",  setLayout   $ XMonad.layoutHook conf)
+         , ("M-n",          refresh)
+         -- cycle through all windows
+         , ("M-j",          windows     $ W.focusDown)
+         , ("M-k",          windows     $ W.focusUp)
+         -- jump to master pane
+         , ("M-m",          windows     $ W.focusMaster)
+         -- moving stuff
+         , ("M-S-j",        windows     $ W.swapDown)
+         , ("M-S-k",        windows     $ W.swapUp)
+         , ("M-S-m",        windows     $ W.swapMaster)
+         -- enlarging|shrinking stuff
+         , ("M-h",          sendMessage $ Shrink)
+         , ("M-l",          sendMessage $ Expand)
+         , ("M-u",          sendMessage $ MirrorShrink)
+         , ("M-i",          sendMessage $ MirrorExpand)
+         -- make floating windows non-floating
+         , ("M-t",          withFocused $ windows . W.sink)
+         -- increase/decrease number of windows in master pane
+         , ("M-,",          sendMessage $ IncMasterN 1)
+         , ("M-.",          sendMessage $ IncMasterN (-1))
+         -- statusbar stuff
+         , ("M-b",          sendMessage $ ToggleStruts)
 
-    -- Keys
-    myKeys = \conf -> mkKeymap conf $
-       [ ("M-<Return>",   spawn       $ term conf)
-       , ("M-C-<Esc>",    spawn       $ "xkill")
-       , ("M-r",          spawn       $ "dmenu_run")
-       , ("M-<Space>",    sendMessage $ NextLayout)
-       , ("M-S-<Space",   setLayout   $ XMonad.layoutHook conf)
-       , ("M-n",          refresh)
-       , ("M-j",          windows     $ W.focusDown)
-       , ("M-k",          windows     $ W.focusUp)
-       , ("M-m",          windows     $ W.focusMaster)
-       , ("M-S-j",        windows     $ W.swapDown)
-       , ("M-S-k",        windows     $ W.swapUp)
-       , ("M-S-m",        windows     $ W.swapMaster)
-       , ("M-h",          sendMessage $ Shrink)
-       , ("M-l",          sendMessage $ Expand)
-       , ("M-t",          withFocused $ windows . W.sink)
-       , ("M-,",          sendMessage $ IncMasterN 1)
-       , ("M-.",          sendMessage $ IncMasterN (-1))
-       , ("M-u",          sendMessage $ MirrorShrink)
-       , ("M-i",          sendMessage $ MirrorExpand)
-       , ("M-b",          sendMessage $ ToggleStruts)
-       , ("M-S-q",        io          $ exitWith ExitSuccess)
-       , ("M-q",          restart "xmonad" True)
+         , ("M-S-q",        io          $ exitWith ExitSuccess)
+         , ("M-q",          restart "xmonad" True)
 
-       -- xf86-keys stuff
-       , ("<XF86HomePage>",         spawn $ "chromium")
-       , ("<XF86Mail>",             spawn $ "chromium")
-       , ("<XF86AudioLowerVolume>", spawn $ amixer "5%-")
-       , ("<XF86AudioRaiseVolume>", spawn $ amixer "5%+")
-       , ("<XF86AudioPrev>",        spawn $ "mpc prev")
-       , ("<XF86AudioStop>",        spawn $ "mpc stop")
-       , ("<XF86AudioPlay>",        spawn $ "mpc toggle")
-       , ("<XF86AudioNext",         spawn $ "mpc next")
+         -- close current window
+         , (prefix "c", kill)
+         -- shortcuts
+         , (prefix "b", spawn $ "chromium")
+         , (prefix "e", spawn $ "gvim")
+         , (prefix "a", spawn $ sTerm $ "alsamixer")
+         , (prefix "o", spawn $ "okular")
+         , (prefix "d", spawn $ "djview")
+         , (prefix "h", spawn $ "kchmviewer")
+         , (prefix "t", namedScratchpadAction scratchpads "htop")
+         , (prefix "r", namedScratchpadAction scratchpads "term")
+         , (prefix "s", namedScratchpadAction scratchpads "skype")
+         ]
+         ++
+         -- M-[asdfgzxcv]   : switch to corresponding workspace
+         -- M-S-[asdfgzxcv] : move window to " " " " " " "
+         [ ( m ++ i, windows $ f i)
+            | i <- myWorkspaces
+            , (m, f) <- [("M-", W.view), ("M-S-", W.shift)]
+         ]
+  where
+    -- prefix that is used in my emacs-like keybidings
+    prefix  = (++) "M-<Tab> " :: String -> String
 
-       , (prefix "c", kill)
-       , (prefix "b", spawn $ "chromium")
-       , (prefix "n", spawn $ ncmpcTerm conf)
-       , (prefix "e", spawn $ "gvim")
-       , (prefix "a", spawn $ sTerm conf $ "alsamixer")
-       , (prefix "t", spawn $ sTerm conf $ "htop")
-       , (prefix "o", spawn $ "okular")
-       , (prefix "d", spawn $ "djview")
-       ]
-       ++
-       -- M-[asdfzxcv]   : switch to corresponding workspace
-       -- M-S-[asdfzxcv] : move window to " " " " " " "
-       [ ( m ++ i, windows $ f j)
-          | (i, j) <- zip (XMonad.workspaces conf) (XMonad.workspaces conf)
-          , (m, f) <- [("M-", W.view), ("M-S-", W.shift)]
-       ]
-    amixer      = (++) "amixer -q set Master "
-    prefix      = (++) "M-<Tab> "
-    term        = XMonad.terminal
-    sTerm c     = (++) (term c ++ " -name urxvt-am -e ")
-    ncmpcTerm c = term c ++ " -name ncmpc -e ncmpc"
+-- Runs console application in terminal without scrollback
+sTerm :: String -> String
+sTerm   = (++) (myTerminal ++ " -name urxvt-am -e ")
 
-    -- colors
-    orange     = "#ee9a00"
-    darkOrange = "#9e4a00"
-    darkGray   = "#3d3d3d"
+scratchpads :: [NamedScratchpad]
+scratchpads = [ NS "htop" (sTerm "htop") (title =? "htop") nonFloating
+              , NS "term" (myTerminal ++ " -name term") (title =? "term")
+                  ( customFloating $ W.RationalRect 0 (2/3) 1 (1/3))
+              , NS "skype" "skype" (className =? "Skype" <&&> role =? "ConversationsWindow")
+                  ( customFloating $ W.RationalRect (1/8) (1/8) (3/4) (3/4))
+              ]
+              where
+                role = stringProperty "WM_WINDOW_ROLE"
 
-    myStartup = do
-      spawn "xsetroot -cursor_name left_ptr -gray"
-      spawn "[ -f ~/.wallpaper ] && feh --bg-tile ~/.wallpaper"
-      spawn "[ -f ~/.Xresources ] && xrdb -merge ~/.Xresources"
-      spawn "setxkbmap -layout us,ru -option grp:caps_toggle"
-      spawn "pgrep kbdd || kbdd"
-      spawn "pgrep urxvtd || urxvtd --opendisplay --fork"
-      runOrRaise "leechcraft" (className =? "Leechcraft")
-      spawn "pgrep stalonetray || stalonetray"
+-- colors
+orange     = "#ee9a00" :: String
+darkOrange = "#9e4a00" :: String
+darkGray   = "#3d3d3d" :: String
