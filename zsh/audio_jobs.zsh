6e4ea5e5d0eb905d78d9f90e681e40d2c04e761d
Michael Pogoda <do0dlez1991@gmail.com>
2014-09-27 23:39:55 +0300

    Rewrite audio_jobs using GNU Parallel.

--- zsh/audio_jobs.zsh
+++ zsh/audio_jobs.zsh
@@ -2,23 +2,14 @@
 # commands used by me to convert audio
 
 function alac2flac() {
-    for alac in *.m4a
-    do
-        alac $alac | flac -o "$(echo $alac | sed -e 's/m4a$/flac/')" -
-        rm -v $alac
-    done
+    find -iname '*.m4a' | parallel --no-notice --eta 'alac {} | flac -o {.}.flac - && rm {}'
 }
 
 function flac2ogg() {
-    oggenc --quality=6.66 *.flac
-    rm -v *.flac
+    find -iname '*.flac' | parallel --no-notice --eta 'oggenc --quality=6.66 {} && rm -v {}'
     collectiongain .
 }
 
 function alac2ogg() {
-    for alac in **/*.m4a
-    do
-        alac $alac | oggenc --quality=6.66 -o "$(echo $alac | sed -e 's/m4a$/ogg/')" -
-        rm -v $alac
-    done
+    find -iname '*.m4a' | parallel --no-notice --eta 'alac {} | oggenc --quality=6.66 -o {.}.ogg - && rm {}'
 }
