477b3c54fd99339c3944bdfd29d86d54e6db5a33
Michael Pogoda <do0dlez1991@gmail.com>
2014-11-24 02:25:28 +0200

    tmux-dashboard: Change log splits into call to multitail.

--- tmux.sh
+++ tmux.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
-SESSION_NAME='dashboard'
+readonly SESSION_NAME='dashboard'
 
-tmux has-session -t $SESSION_NAME
-if [ $? -ne 0 ]
+if ! tmux has-session -t $SESSION_NAME &>/dev/null
 then
-  tmux new-session -d -s $SESSION_NAME -n logs 'sudo tail -f /var/log/messages'
-  tmux split-window -t $SESSION_NAME 'sudo tail -f /var/log/daemon.log'
-  tmux new-window -c ~               -n update  -t $SESSION_NAME
-  tmux new-window                    -n htop    -t $SESSION_NAME htop
-  tmux set-window-option -t $SESSION_NAME:update  allow-rename     off
-  tmux set-window-option -t $SESSION_NAME:update  monitor-activity off
-  tmux set-window-option -t $SESSION_NAME:update  monitor-silence  30
-  tmux set-window-option -t $SESSION_NAME:htop    monitor-activity off
+    readonly LOG_NAMES="/var/log/auth.log \
+                        /var/log/daemon.log \
+                        /var/log/emerge.log \
+                        /var/log/emerge-fetch.log \
+                        /var/log/messages \
+                        /var/log/Xorg.0.log"
+    if which multitail &>/dev/null
+    then
+        tmux new-session -d -s $SESSION_NAME -n logs "sudo multitail $LOG_NAMES"
+    else
+        tmux new-session -d -s $SESSION_NAME -n logs "sudo tail --follow=name $LOG_NAMES"
+    fi
+    tmux new-window -n update  -t $SESSION_NAME
+    tmux new-window -n htop    -t $SESSION_NAME htop
+    tmux set-window-option -t $SESSION_NAME:update  allow-rename     off
+    tmux set-window-option -t $SESSION_NAME:update  monitor-activity off
+    tmux set-window-option -t $SESSION_NAME:update  monitor-silence  30
+    tmux set-window-option -t $SESSION_NAME:htop    monitor-activity off
 fi
 
 tmux attach -t $SESSION_NAME
