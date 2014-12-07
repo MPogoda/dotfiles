23ffb16eec6b0c95ab16704255bbcfaa93496352
Michael Pogoda <do0dlez1991@gmail.com>
2014-12-07 18:52:37 +0200

    Optimise png nicely.

--- zsh/optimise-png.zsh
+++ zsh/optimise-png.zsh
@@ -1,7 +1,7 @@
 #! /bin/zsh
 
 function optpng() {
-    find -iname '*.png' | parallel --no-notice --progress\
+    find -iname '*.png' | nice parallel --no-notice --progress\
         'optipng -o7 -preserve -f0-5 -zc1-9 -zm1-9 -zs0-3'
 }
 
