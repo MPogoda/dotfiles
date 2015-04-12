5c93d08fda58a6675d8d26fb694b078efe425464
Michael Pogoda <do0dlez1991@gmail.com>
2015-04-12 13:56:25 +0300

    Do not spawn a shell inside dashboard-tmux at start.

--- tmux.sh
+++ tmux.sh
@@ -16,11 +16,7 @@ then
         tmux new-session -d -s $SESSION_NAME -n logs "sudo tail --follow=name $LOG_NAMES"
     fi
     tmux split-window -h -t $SESSION_NAME:logs "dmesg -w"
-    tmux new-window -n update  -t $SESSION_NAME
     tmux new-window -n htop    -t $SESSION_NAME htop
-    tmux set-window-option -t $SESSION_NAME:update  allow-rename     off
-    tmux set-window-option -t $SESSION_NAME:update  monitor-activity off
-    tmux set-window-option -t $SESSION_NAME:update  monitor-silence  30
     tmux set-window-option -t $SESSION_NAME:htop    monitor-activity off
 fi
 
