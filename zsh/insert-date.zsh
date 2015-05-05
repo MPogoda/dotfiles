16a0772ddd1b178057d9cb4dbea395fe2d303327
Michael Pogoda <do0dlez1991@gmail.com>
2015-05-05 19:20:11 +0300

    Use prezto as zsh config.

--- zsh/insert-date.zsh
+++
@@ -1,6 +0,0 @@
-# press "ctrl-e d" to insert the actual date in the form yyyy-mm-dd
-insert-datestamp() { LBUFFER+=${(%):-'%D{%Y-%m-%d}'}; }
-zle -N insert-datestamp
-
-#k# Insert a timestamp on the command line (yyyy-mm-dd)
-bindkey '^ed' insert-datestamp
--- zsh/insert-sudo.zsh
+++
@@ -1,8 +0,0 @@
-# insert sudo infront of command
-insert_sudo () {
-  [[ -z $BUFFER ]] && zle up-history
-  [[ $BUFFER != sudo\ * ]] && BUFFER="sudo $BUFFER"
-  zle end-of-line
-}
-
-zle -N insert-sudo insert_sudo
