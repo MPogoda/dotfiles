0486a38fd0eb7d11e31cacb1826206e72f042b74
Mykhailo Pogoda <ext-mykhailo.pogoda@here.com>
2017-12-26 17:53:32 +0200

    tmux-dashboard: start vpn-connection.

--- tmux/dashboard.sh
+++ tmux/dashboard.sh
@@ -5,8 +5,8 @@ if ! tmux has-session -t $SESSION_NAME &>/dev/null
 then
     tmux new-session -d -s $SESSION_NAME -n logs "journalctl --follow"
     tmux new-window -n htop    -t $SESSION_NAME htop
-    tmux split-window -h -t $SESSION_NAME:htop "sudo iotop -a"
     tmux set-window-option -t $SESSION_NAME:htop    monitor-activity off
+    tmux new-window -n openconnect -t $SESSION_NAME "~/.tmux/go-vpn.sh"
 fi
 
 tmux attach -t $SESSION_NAME
