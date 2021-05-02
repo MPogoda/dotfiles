84ec0aa00be26d740c747e4d26c8d8ae37825def
Michael Pogoda <do0dlez1991@gmail.com>
2021-05-03 00:48:18 +0300

    tmux-dashboard: switch order of htop/logs

--- tmux/dashboard.sh
+++ tmux/dashboard.sh
@@ -3,9 +3,10 @@ readonly SESSION_NAME='dashboard'
 
 if ! tmux has-session -t $SESSION_NAME
 then
-    tmux new-session -d -s $SESSION_NAME -n logs "journalctl --follow"
-    tmux new-window -n htop    -t $SESSION_NAME htop
+    tmux new-session -d -s $SESSION_NAME -n htop htop
     tmux set-window-option -t $SESSION_NAME:htop    monitor-activity off
+
+    tmux new-window -n logs    -t $SESSION_NAME "journalctl --follow"
 fi
 
 tmux attach -t $SESSION_NAME
