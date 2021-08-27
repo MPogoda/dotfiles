417d7746043403c98a86129dea848dfd08fba908
Michael Pogoda <do0dlez1991@gmail.com>
2021-08-27 20:05:09 +0300

    Use default indent-blankline char + show current context

--- vim/lua/plugin/indent-blankline.lua
+++ vim/lua/plugin/indent-blankline.lua
@@ -1,8 +1,8 @@
 require('indent_blankline').setup({
-    char = '┊',
     filetype_exclude = { 'help', 'packer' },
     buftype_exclude = { 'terminal', 'nofile' },
     char_highlight = 'LineNr',
     show_trailing_blankline_indent = false,
     use_treesitter = true,
+    show_current_context = true,
 })
