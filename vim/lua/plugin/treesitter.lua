666df2166b61ea26a5bc3f8b4cc3021f399d4f7d
Michael Pogoda <do0dlez1991@gmail.com>
2021-09-05 18:25:15 +0300

    Use maintained treesitter parsers only

--- vim/lua/plugin/treesitter.lua
+++ vim/lua/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-    ensure_installed = 'all',
+    ensure_installed = 'maintained',
     highlight = {
         enable = true, -- false will disable the whole extension
         additional_vim_regex_highlighting = true,
