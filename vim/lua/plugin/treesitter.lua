3919ed7d4e2d0915a5177e5d9282a5caa403320e
Michael Pogoda <do0dlez1991@gmail.com>
2021-09-05 17:58:21 +0300

    Use syntax+treesitter

--- vim/lua/plugin/treesitter.lua
+++ vim/lua/plugin/treesitter.lua
@@ -2,6 +2,7 @@ require('nvim-treesitter.configs').setup({
     ensure_installed = 'all',
     highlight = {
         enable = true, -- false will disable the whole extension
+        additional_vim_regex_highlighting = true,
     },
     incremental_selection = {
         enable = true,
