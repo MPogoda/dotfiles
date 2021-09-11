39ac9903865380c5bb7d6a1666543f018ac09db5
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2021-09-11 16:46:57 +0300

    Disable range formatting for tsserver

--- vim/lua/plugin/lspconfig.lua
+++ vim/lua/plugin/lspconfig.lua
@@ -98,6 +98,7 @@ nvim_lsp.tsserver.setup({
     capabilities = capabilities,
     on_attach = function(client, bufnr)
         client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
 
         local ts_utils = require('nvim-lsp-ts-utils')
         ts_utils.setup({
