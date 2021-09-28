2135d40330c35cea2e5be8c037e5cb35fadbaf8d
Michael Pogoda <do0dlez1991@gmail.com>
2021-09-28 11:59:21 +0300

    nvim: -neoscroll

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -207,15 +207,6 @@ require('packer').startup(function()
     })
 
     use({
-        'karb94/neoscroll.nvim',
-        config = function()
-            require('neoscroll').setup({
-                hide_cursor = false,
-            })
-        end,
-    })
-
-    use({
         'ggandor/lightspeed.nvim',
     })
 
