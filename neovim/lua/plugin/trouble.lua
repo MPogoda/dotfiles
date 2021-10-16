6c8a13fdb73cd9f8f9d2154aedc598bf039c5407
Michael Pogoda <do0dlez1991@gmail.com>
2021-10-16 17:17:09 +0300

    Autoopen+autoclose trouble window when troubles arrive

--- neovim/lua/plugin/trouble.lua
+++ neovim/lua/plugin/trouble.lua
@@ -1,4 +1,7 @@
-require('trouble').setup({})
+require('trouble').setup({
+    auto_open = true,
+    auto_close = true,
+})
 
 require('which-key').register({
     x = { '<cmd>TroubleToggle<cr>', 'Troubles?' },
