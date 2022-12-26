6ee1ef191217165b8d19f1224bf3345f05789438
Michael Pogoda <do0dlez1991@gmail.com>
2022-12-26 14:56:31 +0200

    + mind.nvim

---
+++ neovim/lua/plugins/mind.lua
@@ -0,0 +1,14 @@
+local M = {
+    'phaazon/mind.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = { 'MindOpenMain' },
+    config = function()
+        require('mind').setup()
+    end,
+}
+
+function M.init()
+    vim.keymap.set('n', '<leader>m', require('mind').open_main, { desc = '+MIND' })
+end
+
+return M
