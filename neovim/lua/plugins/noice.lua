fc921d8f9689bcd05f7610ea180ae73256530c73
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2023-02-14 04:33:22 +0200

    Tweak noice.nvim

--- neovim/lua/plugins/noice.lua
+++ neovim/lua/plugins/noice.lua
@@ -20,7 +20,6 @@ function M.config()
         -- you can enable a preset for easier configuration
         presets = {
             bottom_search = true, -- use a classic bottom cmdline for search
-            command_palette = true, -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
             lsp_doc_border = false, -- add a border to hover docs and signature help
         },
