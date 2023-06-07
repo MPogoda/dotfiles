ef58cbfec029b549d5f54491820bdcc8266f69d6
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2023-06-07 14:40:29 +0300

    fold-preview: use opts instead of config function

--- neovim/lua/plugins/fold-preview.lua
+++ neovim/lua/plugins/fold-preview.lua
@@ -1,7 +1,5 @@
 return {
     'anuvyklack/fold-preview.nvim',
     dependencies = { 'anuvyklack/keymap-amend.nvim' },
-    config = function()
-        require('fold-preview').setup({})
-    end,
+    opts = {},
 }
