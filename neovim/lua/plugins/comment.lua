b57f08f730d50ecac4fbb5081408aa66a3f447a2
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2023-06-07 14:24:54 +0300

    comment.nvim -> mini.comment

--- neovim/lua/plugins/comment.lua
+++ neovim/lua/plugins/comment.lua
@@ -1,12 +1,14 @@
 local M = {
-    'numToStr/Comment.nvim',
+    'echasnovski/mini.comment',
+    version = false,
     dependencies = { 'JoosepAlviste/nvim-ts-context-commentstring' },
+    opts = {
+        options = {
+            custom_commentstring = function()
+                return require('ts_context_commentstring.internal').calculate_commentstring({}) or vim.bo.commentstring
+            end,
+        },
+    },
 }
-
-function M.config()
-    require('Comment').setup({
-        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-    })
-end
 
 return M
