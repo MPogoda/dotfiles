8518c966a093ba748ccf2a28b84e003043e68546
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2023-10-23 11:14:48 +0300

    Update indent-blankline to v3 config

--- neovim/lua/plugins/indent-blankline.lua
+++ neovim/lua/plugins/indent-blankline.lua
@@ -1,13 +1,11 @@
 return {
     'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
     event = 'BufReadPre',
     opts = {
-        filetype_exclude = { 'help', 'packer', 'startify', 'Trouble' },
-        buftype_exclude = { 'terminal', 'nofile' },
-        char_highlight = 'LineNr',
-        show_trailing_blankline_indent = false,
-        use_treesitter = true,
-        show_current_context = true,
-        show_current_context_start = true,
+        exclude = {
+            filetypes = { 'help', 'packer', 'startify', 'Trouble' },
+            buftypes = { 'terminal', 'nofile' },
+        },
     },
 }
