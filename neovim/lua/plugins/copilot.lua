be4df2cd04355e68905b81cce55092637b7c97f9
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2023-11-24 12:34:10 +0200

    nvim: change copilot configuration to be more flat

--- neovim/lua/plugins/copilot.lua
+++ neovim/lua/plugins/copilot.lua
@@ -1,13 +1,18 @@
 return {
-    'zbirenbaum/copilot-cmp',
-    dependencies = { 'zbirenbaum/copilot.lua' },
-    cmd = 'Copilot',
-    event = 'InsertEnter',
-    config = function()
-        require('copilot').setup({
+    {
+        'zbirenbaum/copilot-cmp',
+        dependencies = { 'zbirenbaum/copilot.lua' },
+        cmd = 'Copilot',
+        event = 'InsertEnter',
+        config = true,
+    },
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        event = 'InsertEnter',
+        opts = {
             suggestion = { enabled = false },
             panel = { enabled = false },
-        })
-        require('copilot_cmp').setup()
-    end,
+        },
+    },
 }
