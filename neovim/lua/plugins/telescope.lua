3879fc27b390be9b58b9ad2a8192c9b2ed0333fe
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2024-03-05 12:51:32 +0200

    nvim: use vim.keymap.set for telescope

--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -2,7 +2,6 @@ local M = {
     'nvim-telescope/telescope.nvim',
     cmd = { 'Telescope' },
     dependencies = {
-        'folke/which-key.nvim',
         'nvim-lua/plenary.nvim',
         'gbrlsnchs/telescope-lsp-handlers.nvim',
         'nvim-tree/nvim-web-devicons',
@@ -31,41 +30,33 @@ function M.config()
 end
 
 function M.init()
-    require('which-key').register({
-        f = {
-            name = '+find',
-            b = {
-                function()
-                    require('telescope.builtin').buffers({ theme = 'get_dropdown' })
-                end,
-                'Buffers',
-            },
-            f = {
-                function()
-                    require('telescope.builtin').find_files({ theme = 'get_dropdown' })
-                end,
-                'Files',
-            },
-            g = {
-                function()
-                    require('telescope.builtin').git_status({ theme = 'get_dropdown' })
-                end,
-                'Git status',
-            },
-            o = {
-                function()
-                    require('telescope.builtin').oldfiles()
-                end,
-                'Oldfiles',
-            },
-            ['/'] = {
-                function()
-                    require('telescope.builtin').grep_string()
-                end,
-                'Grep string',
-            },
-        },
-    }, { prefix = '<leader>' })
+    vim.keymap.set('n', '<leader>fb', function()
+        require('telescope.builtin').buffers({ theme = 'get_dropdown' })
+    end, { desc = 'Buffers' })
+    vim.keymap.set('n', '<leader>ff', function()
+        require('telescope.builtin').find_files({ theme = 'get_dropdown' })
+    end, { desc = 'Files' })
+    vim.keymap.set('n', '<leader>fg', function()
+        require('telescope.builtin').git_status({ theme = 'get_dropdown' })
+    end, { desc = 'Git status' })
+    vim.keymap.set('n', '<leader>fo', function()
+        require('telescope.builtin').oldfiles()
+    end, { desc = 'Oldfiles' })
+    vim.keymap.set('n', '<leader>f/', function()
+        require('telescope.builtin').grep_string()
+    end, { desc = 'Grep string' })
+    vim.keymap.set('n', '<leader>fr', function()
+        require('telescope.builtin').resume()
+    end, { desc = 'Resume' })
+    vim.keymap.set('n', '<leader>fk', function()
+        require('telescope.builtin').keymaps()
+    end, { desc = 'Keymaps' })
+    vim.keymap.set('n', '<leader>fh', function()
+        require('telescope.builtin').help_tags()
+    end, { desc = 'Help' })
+    vim.keymap.set('n', '<leader>fd', function()
+        require('telescope.builtin').diagnostics()
+    end, { desc = 'Help' })
 end
 
 return M
