1b4b58caec1d35aa372171e435e1316fbc4b5aa3
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2024-03-05 12:51:10 +0200

    nvim: update configuration for lua_ls

--- neovim/lua/plugins/lspconfig.lua
+++ neovim/lua/plugins/lspconfig.lua
@@ -101,7 +101,25 @@ function M.config()
             json = { format = { enable = true } },
         },
         rust_analyzer = {},
-        lua_ls = {},
+        lua_ls = {
+            settings = {
+                runtime = { version = 'LuaJIT' },
+                workspace = {
+                    checkThirdParty = false,
+                    -- Tells lua_ls where to find all the Lua files that you have loaded
+                    -- for your neovim configuration.
+                    library = {
+                        '${3rd}/luv/library',
+                        unpack(vim.api.nvim_get_runtime_file('', true)),
+                    },
+                    -- If lua_ls is really slow on your computer, you can try this instead:
+                    -- library = { vim.env.VIMRUNTIME },
+                },
+                completion = {
+                    callSnippet = 'Replace',
+                },
+            },
+        },
         hls = {},
         tsserver = {},
     }
