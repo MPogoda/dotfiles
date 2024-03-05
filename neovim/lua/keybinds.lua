8c2746820cd41ff59cd9934c00e3db80cc80556b
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2024-03-05 12:52:58 +0200

    nvim: use plain vim.keymap.set for keybinds

--- neovim/lua/keybinds.lua
+++ neovim/lua/keybinds.lua
@@ -1,44 +1,26 @@
-require('which-key').register({
-    ['$'] = { "<cmd>execute 'bnext<bar>bdelete' bufnr('%')<cr>", 'Close current buffer' },
-    e = { '<cmd>edit!<cr>', 'Reload' },
-    w = { '<cmd>write<cr>', 'Save' },
-    t = {
-        name = '+toggle',
-        s = {
-            function()
-                vim.wo.spell = not vim.wo.spell
-            end,
-            'Spell?',
-        },
-    },
-}, {
-    prefix = '<leader>',
-})
+vim.keymap.set('n', '<leader>$', "<cmd>execute 'bnext<bar>bdelete' bufnr('%')<cr>", { desc = 'Close current buffer!' })
+vim.keymap.set('n', '<leader>e', '<cmd>edit!<cr>', { desc = 'R[e]load' })
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = '[W]rite' })
+
+vim.keymap.set('n', '<leader>ts', function()
+    vim.wo.spell = not vim.wo.spell
+end, { desc = 'Spell?' })
 
 vim.keymap.set('n', 'Y', 'y$', { noremap = true })
 
 vim.keymap.set('i', 'jj', '<esc>', { silent = true, noremap = true })
 vim.keymap.set('c', 'jj', '<esc>', {})
 
-vim.keymap.set('n', '<c-l>', ':<c-u>nohlsearch<cr>', { noremap = true })
-
 vim.keymap.set('n', 'gv', '`[v`]', { noremap = true }) -- last edited or pasted
 
 vim.keymap.set('v', '>', '>gv', { noremap = true })
 vim.keymap.set('v', '<', '<gv', { noremap = true })
 
--- Move to window using the <ctrl> movement keys
-vim.keymap.set('n', '<left>', '<C-w>h')
-vim.keymap.set('n', '<down>', '<C-w>j')
-vim.keymap.set('n', '<up>', '<C-w>k')
-vim.keymap.set('n', '<right>', '<C-w>l')
--- Resize window using <ctrl> arrow keys
-vim.keymap.set('n', '<S-Up>', '<cmd>resize +2<CR>')
-vim.keymap.set('n', '<S-Down>', '<cmd>resize -2<CR>')
-vim.keymap.set('n', '<S-Left>', '<cmd>vertical resize -2<CR>')
-vim.keymap.set('n', '<S-Right>', '<cmd>vertical resize +2<CR>')
-
 vim.keymap.set('n', '<C-Left>', '<cmd>bprevious<cr>')
 vim.keymap.set('n', '<C-Right>', '<cmd>bnext<cr>')
 
-vim.keymap.set({ 'i', 'n' }, '<esc>', '<cmd>noh<cr><esc>')
+vim.keymap.set({ 'i', 'n' }, '<esc>', '<cmd>nohlsearch<cr><esc>')
+
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>te', vim.diagnostic.open_float, { desc = 'Show diagnostic [e]rror message' })
