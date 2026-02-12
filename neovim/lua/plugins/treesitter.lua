19d75ecf4d7c19e5331c2ad1d7c7d3f38d0ce0c5
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2026-02-12 13:30:29 +0200

    ignore problematic ipkg treesitter

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -9,6 +9,7 @@ local M = {
 function M.config()
     require('nvim-treesitter.configs').setup({
         ensure_installed = 'all',
+        ignore_install = { 'ipkg' },
         highlight = { enable = true },
         incremental_selection = {
             enable = true,
