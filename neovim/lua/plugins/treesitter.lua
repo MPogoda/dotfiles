f6333c7ff84c962e0d41800384b5fa17c41aad35
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2026-04-01 12:39:34 +0300

    switch to nvim-treesitter@main

--- neovim/lua/plugins/treesitter.lua
+++ neovim/lua/plugins/treesitter.lua
@@ -1,36 +1,52 @@
 local M = {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    event = 'BufReadPost',
+    lazy = false,
     dependencies = {
         'andersevenrud/nvim_context_vt',
     },
 }
 function M.config()
-    require('nvim-treesitter.configs').setup({
-        ensure_installed = {
-            'typescript', 'tsx', 'javascript', 'jsdoc',
-            'lua', 'luadoc', 'luap',
-            'python',
-            'rust',
-            'html', 'css', 'json', 'jsonc', 'yaml', 'toml',
-            'bash', 'diff', 'regex', 'query',
-            'markdown', 'markdown_inline',
-            'vim', 'vimdoc',
-            'gitcommit', 'git_rebase', 'gitignore',
-            'dockerfile', 'haskell',
-        },
-        highlight = { enable = true },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = 'gnn',
-                node_incremental = 'grn',
-                scope_incremental = 'grc',
-                node_decremental = 'grm',
-            },
-        },
-        indent = { enable = true },
+    local languages = {
+        'typescript',
+        'tsx',
+        'javascript',
+        'jsdoc',
+        'lua',
+        'luadoc',
+        'luap',
+        'python',
+        'rust',
+        'html',
+        'css',
+        'json',
+        'yaml',
+        'toml',
+        'bash',
+        'diff',
+        'regex',
+        'query',
+        'markdown',
+        'markdown_inline',
+        'vim',
+        'vimdoc',
+        'gitcommit',
+        'git_rebase',
+        'gitignore',
+        'dockerfile',
+        'haskell',
+    }
+
+    require('nvim-treesitter').install(languages)
+    vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('treesitter-extra', { clear = true }),
+        pattern = languages,
+        callback = function()
+            vim.treesitter.start()
+            vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            vim.wo.foldmethod = 'expr'
+            vim.bo.indentexpr = "v:lua.require('nvim-treesitter').indentexpr()"
+        end,
     })
 end
 
