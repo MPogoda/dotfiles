57f5a5da5c13a0cd5b23a7f0e4517e51e85f1e43
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2026-04-10 04:36:58 +0300

    some more ui2 niceness

--- neovim/init.lua
+++ neovim/init.lua
@@ -94,24 +94,72 @@ require('keybinds')
 require('qfutils')
 require('neovide')
 
-require('vim._core.ui2').enable({
+local ui2 = require('vim._core.ui2')
+ui2.enable({
     enable = true,
     msg = {
-        targets = 'msg',
-        cmd = { -- Options related to messages in the cmdline window.
-            height = 0.5, -- Maximum height while expanded for messages beyond 'cmdheight'.
+        targets = {
+            [''] = 'msg',
+            empty = 'cmd',
+            bufwrite = 'msg',
+            confirm = 'cmd',
+            emsg = 'pager',
+            echo = 'msg',
+            echomsg = 'msg',
+            echoerr = 'pager',
+            completion = 'cmd',
+            list_cmd = 'pager',
+            lua_error = 'pager',
+            lua_print = 'msg',
+            progress = 'pager',
+            rpc_error = 'pager',
+            quickfix = 'msg',
+            search_cmd = 'cmd',
+            search_count = 'cmd',
+            shell_cmd = 'pager',
+            shell_err = 'pager',
+            shell_out = 'pager',
+            shell_ret = 'msg',
+            undo = 'msg',
+            verbose = 'pager',
+            wildlist = 'cmd',
+            wmsg = 'msg',
+            typed_cmd = 'cmd',
         },
-        dialog = { -- Options related to dialog window.
-            height = 0.5, -- Maximum height.
-        },
-        msg = { -- Options related to msg window.
-            height = 0.5, -- Maximum height.
-            timeout = 4000, -- Time a message is visible in the message window.
-        },
-        pager = { -- Options related to message window.
-            height = 1, -- Maximum height.
-        },
+        cmd = { height = 0.5 },
+        dialog = { height = 0.5 },
+        msg = { height = 0.3, timeout = 5000 },
+        pager = { height = 0.5 },
     },
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = 'msg',
+    callback = function()
+        local win = ui2.wins and ui2.wins.msg
+        if win and vim.api.nvim_win_is_valid(win) then
+            vim.api.nvim_set_option_value(
+                'winhighlight',
+                'Normal:NormalFloat,FloatBorder:FloatBorder',
+                { scope = 'local', win = win }
+            )
+        end
+    end,
+})
+
+local msgs = require('vim._core.ui2.messages')
+local orig_set_pos = msgs.set_pos
+msgs.set_pos = function(tgt)
+    orig_set_pos(tgt)
+    if (tgt == 'msg' or tgt == nil) and vim.api.nvim_win_is_valid(ui2.wins.msg) then
+        pcall(vim.api.nvim_win_set_config, ui2.wins.msg, {
+            relative = 'editor',
+            anchor = 'NE',
+            row = 1,
+            col = vim.o.columns - 1,
+            border = 'rounded',
+        })
+    end
+end
 
 vim.lsp.document_color.enable(true, nil, { style = 'virtual' })
