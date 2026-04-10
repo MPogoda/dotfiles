a08abb8c089e5f5980dfab9a3331a29396c6cb14
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2026-04-11 01:24:40 +0300

    move ui2 popup back to SE

--- neovim/init.lua
+++ neovim/init.lua
@@ -153,11 +153,8 @@ msgs.set_pos = function(tgt)
     orig_set_pos(tgt)
     if (tgt == 'msg' or tgt == nil) and vim.api.nvim_win_is_valid(ui2.wins.msg) then
         pcall(vim.api.nvim_win_set_config, ui2.wins.msg, {
-            relative = 'editor',
-            anchor = 'NE',
-            row = 1,
-            col = vim.o.columns - 1,
             border = 'rounded',
+            anchor = 'SE',
         })
     end
 end
