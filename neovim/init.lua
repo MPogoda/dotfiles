00caa8e0d00af89575cd2739660682547e31f62c
Mykhailo Pohoda <mykhailo.pohoda@bolt.eu>
2026-04-11 20:25:56 +0300

    Extract ui2 stuff into separate file

--- neovim/init.lua
+++ neovim/init.lua
@@ -94,70 +94,7 @@ require('keybinds')
 require('qfutils')
 require('neovide')
 
-local ui2 = require('vim._core.ui2')
-ui2.enable({
-    enable = true,
-    msg = {
-        targets = {
-            [''] = 'msg',
-            empty = 'cmd',
-            bufwrite = 'msg',
-            confirm = 'cmd',
-            emsg = 'pager',
-            echo = 'msg',
-            echomsg = 'msg',
-            echoerr = 'pager',
-            completion = 'cmd',
-            list_cmd = 'pager',
-            lua_error = 'pager',
-            lua_print = 'msg',
-            progress = 'pager',
-            rpc_error = 'pager',
-            quickfix = 'msg',
-            search_cmd = 'cmd',
-            search_count = 'cmd',
-            shell_cmd = 'pager',
-            shell_err = 'pager',
-            shell_out = 'pager',
-            shell_ret = 'msg',
-            undo = 'msg',
-            verbose = 'pager',
-            wildlist = 'cmd',
-            wmsg = 'msg',
-            typed_cmd = 'cmd',
-        },
-        cmd = { height = 0.5 },
-        dialog = { height = 0.5 },
-        msg = { height = 0.3, timeout = 5000 },
-        pager = { height = 0.5 },
-    },
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'msg',
-    callback = function()
-        local win = ui2.wins and ui2.wins.msg
-        if win and vim.api.nvim_win_is_valid(win) then
-            vim.api.nvim_set_option_value(
-                'winhighlight',
-                'Normal:NormalFloat,FloatBorder:FloatBorder',
-                { scope = 'local', win = win }
-            )
-        end
-    end,
-})
-
-local msgs = require('vim._core.ui2.messages')
-local orig_set_pos = msgs.set_pos
-msgs.set_pos = function(tgt)
-    orig_set_pos(tgt)
-    if (tgt == 'msg' or tgt == nil) and vim.api.nvim_win_is_valid(ui2.wins.msg) then
-        pcall(vim.api.nvim_win_set_config, ui2.wins.msg, {
-            border = 'rounded',
-            anchor = 'SE',
-        })
-    end
-end
+require('ui2')
 
 vim.lsp.document_color.enable(true, nil, { style = 'virtual' })
 
